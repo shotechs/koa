d33ee65cf016169948e82308ca8e58c832876104
shotechs <shotechs@gmail.com>
2020-09-21 18:26:45 -0400

    first commit

---
+++ my-koa-app.js
@@ -0,0 +1,31 @@
+//require("babel-register");
+// require the rest of the app that needs to be transpiled after the hook
+//const app = require("./app");
+
+const Koa = require("koa");
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// response
+
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
+});
+
+app.listen(3000);
